39a0624f5bc020879965ebcd61cc15cbc3a54863
Alexis <panchaponal18@gmail.com>
2023-11-08 08:33:15 -0500

    inicio dia08/11

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -3,10 +3,13 @@ export default class GameOverScene extends Phaser.Scene {
         super({ key: 'GameOver' });
     }
 
+    preload(){
+        this.add.image('over', 'assets/gameOver.png')
+    }
+
     create() {
-        // Puedes mostrar un mensaje de game over
-        const gameOverText = this.add.text(400, 300, '¡Game Over!', { fontSize: '48px', fill: 'red' });
-        gameOverText.setOrigin(0.5);
+        
+        const backgroundCorrecto = this.add.image(400, 300, 'over').setScale(0.42, 0.56);
 
         // Agrega un botón para volver al menú principal o volver a intentar
         const backButton = this.add.text(400, 400, 'Volver al Menú', { fontSize: '24px', fill: 'blue' });
